f3c83bedc8ae1c27edc6f3b98dd09436ce1f9c1e
Warefoxe <jurassic151@gmail.com>
2017-12-23 00:03:57 +0200

    utf

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,272 @@
+﻿#include<iostream>
+#include <iomanip>
+
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+
+
+	/*int a;
+	cout << "1.Red\n" << "2.Yellow\n" "3.Green\n" << endl;
+	cin >> a;
+	if (a == 1) {
+	cout << "Stop" << endl;
+	}
+
+	else if (a == 2) {
+	cout << "Wait" << endl;
+	}
+
+	else if (a == 3) {
+	cout << "Go" << endl;
+	}
+
+	else
+	{
+	cout << "Error" << endl;
+	}*/
+
+
+
+	/*int Color;
+	cout << "1.Red\n" << "2.Yellow\n" "3.Green\n" << endl;
+	cin >> Color;
+
+	switch (Color)
+	{
+	case 1:
+	cout << "Stop" << endl;
+	break;
+	case 2:
+	cout << "Wait" << endl;
+	break;
+	case 3:
+	cout << "Go" << endl;
+	break;
+	default:
+	cout << "Wrong color" << endl;
+	break;
+	}*/
+	
+
+	//int a;
+	//cout << "Введiть число ";
+	//cin >> a;
+	////a = a % 2 == 0;
+
+	//switch (a%2)
+	//{
+	//case 0:
+
+	//cout << "Парне"  << endl;
+	//break;
+	//default :
+	//cout << "Не парне" << endl;
+	//break;
+	//}
+
+
+
+	/*int a;
+	cout << "Введiть номер мiсяця : ";
+	cin >> a;
+
+	switch (a)
+	{
+	case 1 : case 2 : case 12:
+		cout << "Зима" << endl;
+		break;
+	case 3: case 4: case 5:
+		cout << "Весна" << endl;
+		break;
+	case 6: case 7: case 8:
+		cout << "Лiто" << endl;
+		break;
+	case 9: case 10: case 11:
+		cout << "Осiнь" << endl;
+		break;
+	default:
+		cout << "Не правильно" << endl;
+		break;
+	}*/
+
+
+	/*int d;
+
+	cout << "Введiте день тижня (1-7): ";
+	cin >> d;
+
+	switch (d)
+	{
+	case 1:
+		cout << "Понедiлок: \n8:00 Навчання \n18:00 Шаг \n";
+		break;
+	case 2:
+		cout << "Вiвторок: \n8:00 Навчання \n18:00 Шаг \n";
+		break;
+	case 3:
+		cout << "Середа: \n8:00 Навчання \n19:00 Шаг \n";
+		break;
+	case 4:
+		cout << "Четвер: \n8:00 Навчання \n19:00 Тренування \n";
+		break;
+	case 5:
+		cout << "П'ятниця: \n8:00 Навчання \n17:00 Тренування \n";
+		break;
+	case 6:
+		cout << "Субота: \nФутбол \n";
+		break;
+	case 7:
+		cout << "Недiля: \nВiдпочинок \n";
+		break;
+	default:
+		cout << "Немає такого дня в тижнi " << endl;
+
+	}*/
+
+
+
+	/*int  a;
+	float dt , dov;
+	cout << "Введiте довжину в сантиметрах = " << endl;
+	cin >>dov;
+
+	cout << "Перевести довжину в " << "\n1.Метри" << "\n2.Дециметри" << "\n3.Мiлiметри" << endl;
+	cin >> a;
+
+
+	switch (a)
+	{
+	case 1:
+		cout << dov << " в метрах = " << dov / 100 << endl;
+		break;
+	case 2:
+		cout << dov << " в дециметрах = " << dov / 10 << endl;
+		break;
+	case 3:
+		cout << dov << " в мiлiметрах = " << dov * 10 << endl;
+		break;
+	default:
+	break;
+	}*/
+	
+
+	/*double fchislo, schislo , res;
+	char operation;
+
+	cout << "Введiть перше число : " << endl;
+	cin >> fchislo;
+
+	cout << "Виберiть операцiю " << "\n + " << "\n - " << "\n / " << "\n * \n" << endl;
+	cin >> operation;
+
+	cout << "Введiть друге число : " << endl;
+	cin >> schislo;
+
+
+
+	switch (operation)
+	{
+	case '+':
+	res = fchislo + schislo;
+	cout << fchislo << " + " << schislo << " = " << res << endl;
+	break;
+	case '-':
+	res = fchislo - schislo;
+	cout << fchislo << " - " << schislo << " = " << res << endl;
+	break;
+	case '/':
+	res = fchislo / schislo;
+	if (schislo == 0)
+	{
+	cout << "На 0 дiлити не можна " << endl;
+	}
+	else
+	{
+	cout << fchislo << " / " << schislo << " = " << res << endl;
+	}
+
+	break;
+	case '*':
+	res = fchislo * schislo;
+	cout << fchislo << " * " << schislo << " = " << res << endl;
+	break;
+	default:
+	cout << "Помилка" << endl;
+	break;
+	}*/
+
+
+
+	/*char symvol;
+	cout << "Введiть символ" << "\n + " << "\n - " << "\n / " << "\n * " << "\n , " << "\n . " << "\n : " << "\n ; \n" << endl;
+	cin >> symvol;
+
+	switch (symvol)
+	{
+	case '+': case '-': case '/': case '*':
+		cout << "Знак арифметичної операцiї " << endl;
+		break;
+	case ',': case '.': case ':': case ';':
+		cout << "Є роздiловим знаком " << endl;
+		break;
+	default:
+		cout << "Помилка " << endl;
+		break;
+	}*/
+	
+	
+	/*char symbol;
+	cout << "N –пiвнiч, S - пiвдень, E – схiд, W – захiд " << endl;
+	cin >> symbol;
+
+	switch (symbol)
+	{
+	case 'N':case 'W':case 'S':
+		break;
+	
+	
+	case 'E': cout << "Направо " << endl;
+		break;
+	
+		
+	default:
+		cout << "Errr " << endl;
+		break;
+	}*/
+
+
+
+
+	enum Operator { Kyivstar = 1, МТС, Lifecell, Intertelecom, TriMob };
+	int c_operat;
+	float t;
+	cout << "Введiть кiльскiсть хвилин " << endl;
+	cin >>t;
+	cout << "Виберiть оператора (1-Kyivstar, 2-МТС, 3-Lifecell, 4-Intertelecom, 5-TriMob ): " << endl;
+	cin >> c_operat;
+
+	switch (c_operat)
+	{
+		
+	case Kyivstar:
+		cout << "Вартiсть " << t << " хвилин = " << t * 3.50 << " грн. ";
+		break;
+	case МТС:
+		cout << "Вартiсть: " << t << " хвилин = " << t * 2.50 << " грн. ";
+		break;
+	case Lifecell:
+		cout << "Вартiсть: " << t << " хвилин = " << t * 3 << " грн. ";		break;
+	case Intertelecom:
+		cout << "Вартiсть: " << t << " хвилин = " << t * 4 << " грн. ";		break;
+	case TriMob:
+		cout << "Вартiсть: " << t << " хвилин = " << t * 4.50 << " грн. ";		break;
+	default:
+		cout << "Wrong operation\n";
+		break;
+	}
+	system("pause");
+	return 0;
+}
